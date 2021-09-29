12d50ac5a0985b892d30ccf24cd1c12878bc400a
Scott W Harden <swharden@gmail.com>
2021-09-29 12:17:48 -0400

    SciTIF

---
+++ tools/SciTIF/src/SciTIF.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace SciTIF.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
