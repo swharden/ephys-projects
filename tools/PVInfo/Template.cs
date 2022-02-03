26b1bfc57b134104a96a510be13fdf2bae5c3618
Scott W Harden <swharden@gmail.com>
2022-02-03 16:15:15 -0500

    template: improve location system

---
+++ tools/PVInfo/Template.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PVInfo
+{
+    internal static class Template
+    {
+        const string FILENAME = "template.html";
+
+        public static string HTML => File.ReadAllText(Locate());
+
+        private static string Locate(int maxLevelsUp = 10)
+        {
+            string folder = Path.GetFullPath("./");
+
+            for (int i = 0; i < maxLevelsUp; i++)
+            {
+                string path = Path.Combine(folder, FILENAME);
+
+                if (File.Exists(path))
+                    return path;
+
+                folder = Path.GetDirectoryName(folder);
+            }
+
+            throw new InvalidOperationException($"unable to locate {FILENAME} in {folder}");
+        }
+    }
+}
