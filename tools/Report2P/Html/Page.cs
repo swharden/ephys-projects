5e3d37729ff424b6dae7baf6f8c66dc19d323cfb
Scott W Harden <swharden@gmail.com>
2022-03-18 15:36:24 -0400

    Report2P: prune obsolete code

---
+++ tools/Report2P/Html/Page.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Report2P.Html;
+
+public class Page
+{
+    public string Title { get; set; } = string.Empty;
+    public string Subtitle { get; set; } = string.Empty;
+
+    public readonly StringBuilder Content = new();
+
+    public Page()
+    {
+
+    }
+
+    public void Save(string path)
+    {
+        path = Path.GetFullPath(path);
+        HtmlTemplates.ThrowIfNotPopulated();
+
+        StringBuilder sb = new();
+        sb.Append("<div class='border bg-light shadow p-3'>");
+
+        sb.Append($"<h1>{Title}</h1>");
+        sb.Append($"<div><code>{Subtitle}</code></div>");
+
+        sb.Append("</div>");
+
+        string html = HtmlTemplates.BuildPage(Title, sb.ToString());
+        File.WriteAllText(path, html);
+        Console.WriteLine($"  wrote: {path}");
+    }
+}
--- tools/Report2P/TimelinePage.cs
+++ tools/Report2P/TimelinePage.cs
@@ -1,13 +1,134 @@
 ﻿using Report2P.Experiment;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Report2P;
 
-internal static class TimelinePage
+internal class TimelinePage
 {
-    public static void CreateIndex(IExperiment[] experiments, string folder2p)
+    public static void MakeIndex(string folderOf2pFolders)
     {
+        List<TimelineItem> timelineItems = new();
+        timelineItems.AddRange(GetTimelineItems2P(folderOf2pFolders));
+        timelineItems.AddRange(GetTimelineItemsAbf(folderOf2pFolders));
+
+        MakeIndexPage(folderOf2pFolders, timelineItems.ToArray());
+    }
+
+    private static TimelineItem[] GetTimelineItemsAbf(string folderOf2pFolders)
+    {
+        string abfFolder = Path.GetFullPath(Path.Combine(folderOf2pFolders, "../abfs"));
+        Console.WriteLine(abfFolder);
+        if (!Directory.Exists(abfFolder))
+            return Array.Empty<TimelineItem>();
+
+        List<TimelineItem> timelineItems = new();
+
+        foreach (string abfPath in Directory.GetFiles(abfFolder, "*.abf").Where(x => x.EndsWith(".abf")))
+        {
+            Console.WriteLine($"Analyzing: {abfPath}");
+            AbfSharp.ABFFIO.ABF abf = new(abfPath, preloadSweepData: false);
+
+            TimelineItem item = new()
+            {
+                Title = Path.GetFileName(abfPath),
+                Content = $"<pre>{abf}</pre><br><br>abf auto-analysis images will go here",
+                DateTime = GetAbfDateTime(abf),
+                Icon = "abf",
+            };
+
+            timelineItems.Add(item);
+        }
+
+        return timelineItems.ToArray();
+    }
+
+    private static TimelineItem[] GetTimelineItems2P(string folderOf2pFolders)
+    {
+        List<TimelineItem> timelineItems = new();
+
+        foreach (string folder in Directory.GetDirectories(folderOf2pFolders))
+        {
+            try
+            {
+                IExperiment experiment = ExperimentFactory.GetExperiment(folder);
+                Console.WriteLine($"Analyzing: {experiment.Path}");
+                experiment.Analyze();
+
+                TimelineItem item = new()
+                {
+                    Title = Path.GetFileName(experiment.Path),
+                    Content = experiment.Details,
+                    DateTime = experiment.DateTime,
+                    Icon = "abf",
+                    ImageGroups = experiment.ImageGroups.ToArray(),
+                };
+
+                timelineItems.Add(item);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        return timelineItems.ToArray();
+    }
+
+    private static void MakeIndexPage(string folderOf2pFolders, TimelineItem[] timelineItems)
+    {
+        TimelineItem[] sortedTimelineItems = timelineItems.OrderBy(x => x.DateTime).ToArray(); ;
+
+        string templateFolder = "../../../Templates";
+        Html.ReportBuilder page = new(templateFolder, folderOf2pFolders);
+
+        page.DivStart("my-5");
+        DateTime lastItemTime = sortedTimelineItems.First().DateTime;
+        DateTime experimentStartTime = sortedTimelineItems.First().DateTime;
+
+        foreach (TimelineItem item in sortedTimelineItems)
+        {
+            if (item.DateTime - lastItemTime > TimeSpan.FromMinutes(10))
+            {
+                page.Add(new TimelineItems.Spacer());
+                lastItemTime = item.DateTime;
+                experimentStartTime = item.DateTime;
+            }
+
+            item.ExperimentTime = item.DateTime - experimentStartTime;
+            page.Add(item);
+        }
+        page.DivEnd();
+
+        string outputFilePath = Path.Combine(folderOf2pFolders, "index.html");
+        page.Save(outputFilePath);
+        Console.WriteLine(outputFilePath);
+    }
+
+    private static DateTime GetAbfDateTime(AbfSharp.ABFFIO.ABF abf)
+    {
+        int datecode = (int)abf.Header.uFileStartDate;
+
+        int day = datecode % 100;
+        datecode /= 100;
+
+        int month = datecode % 100;
+        datecode /= 100;
+
+        int year = datecode;
+
+        try
+        {
+            if (year < 1980 || year >= 2080)
+                throw new InvalidOperationException("unexpected creation date year in header");
+            return new DateTime(year, month, day).AddMilliseconds(abf.Header.uFileStartTimeMS);
+        }
+        catch
+        {
+            return new DateTime(0);
+        }
     }
 }
