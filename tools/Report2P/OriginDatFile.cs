e1c15e43f217b162eea0e52aa259b63f99694e75
Scott W Harden <swharden@gmail.com>
2022-05-05 18:23:14 -0400

    Report2P: origin export of single channel data

--- tools/Report2P/OriginDatFile.cs
+++ tools/Report2P/OriginDatFile.cs
@@ -21,7 +21,9 @@ namespace Report2P
             sb.AppendLine($"{fps:N3} FPS\timage mean\timage mean");
             for (int i = 0; i < xs.Length; i++)
             {
-                sb.AppendLine($"{xs[i]}\t{red[i]}\t{green[i]}");
+                double r = i < red.Length ? red[i] : 0;
+                double g = i < green.Length ? green[i] : 0;
+                sb.AppendLine($"{xs[i]}\t{r}\t{g}");
             }
 
             File.WriteAllText(filePath, sb.ToString());
