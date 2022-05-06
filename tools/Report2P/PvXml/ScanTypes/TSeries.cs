293e54131d1f602a475b035c0df369bbe9cd6e56
Scott W Harden <swharden@gmail.com>
2022-05-06 17:06:56 -0400

    Report2P: show resolution

--- tools/Report2P/PvXml/ScanTypes/TSeries.cs
+++ tools/Report2P/PvXml/ScanTypes/TSeries.cs
@@ -14,6 +14,8 @@ public class TSeries : IScan
     public int FrameCount => FrameTimes.Length;
     public double FrameAcquisitionTime => FrameTimes.Length >= 2 ? FrameTimes[1] - FrameTimes[0] : double.NaN;
     public double TotalAcquisitionTime => FrameAcquisitionTime * FrameCount;
+    public readonly int Width;
+    public readonly int Height;
 
     public TSeries(string path)
     {
@@ -21,11 +23,13 @@ public class TSeries : IScan
         XDocument xmlDoc = PVState.XmlDoc;
         AssertValidScan(xmlDoc);
         FrameTimes = GetFrameTimes(xmlDoc);
+        (Width, Height) = GetDimensions(xmlDoc);
     }
 
     public string GetSummary()
     {
         StringBuilder sb = new(PVState.GetSummary());
+        sb.AppendLine($"TSeries Image resolution: {Width}x{Height}");
         sb.AppendLine($"TSeries Image count: {FrameCount}");
         sb.AppendLine($"TSeries Image time: {FrameAcquisitionTime} sec ({1.0 / FrameAcquisitionTime:N2} FPS)");
         sb.AppendLine($"TSeries Total time: {TotalAcquisitionTime} ({Math.Round(TotalAcquisitionTime / 60, 3)} min)");
@@ -53,5 +57,29 @@ public class TSeries : IScan
            .Select(x => x.Attribute("relativeTime").Value)
            .Select(x => double.Parse(x))
            .ToArray();
+    }
+
+    public static (int width, int height) GetDimensions(XDocument xmlDoc)
+    {
+        int width = 0;
+        int height = 0;
+
+        foreach (XElement element in xmlDoc.XPathSelectElements("PVScan/PVStateShard/PVStateValue"))
+        {
+            foreach (XAttribute keyAttribute in element.Attributes("key"))
+            {
+                XAttribute? valueAttribute = element.Attribute("value");
+                if (valueAttribute is null)
+                    continue;
+
+                if (keyAttribute.Value == "linesPerFrame")
+                    width = int.Parse(valueAttribute.Value);
+
+                if (keyAttribute.Value == "pixelsPerLine")
+                    height = int.Parse(valueAttribute.Value);
+            }
+        }
+
+        return (width, height);
     }
 }
