f5036061909bf36b063b96f7638a4f738eef6023
Scott W Harden <swharden@gmail.com>
2022-05-13 15:26:57 -0400

    Report2P: support command line arguments

--- tools/Report2P/Program.cs
+++ tools/Report2P/Program.cs
@@ -2,25 +2,45 @@
 
 public static class Program
 {
-    public static void Main()
+    public static void Main(string[] args)
+    {
+        if (System.Diagnostics.Debugger.IsAttached)
+        {
+            DeveloperAnalysis();
+            return;
+        }
+        else if (args.Length == 1)
+        {
+            Console.WriteLine("Analyzing subfolders in: {}");
+            Analysis.AnalyzeAllSubfolders(args[1], overwrite: false);
+            TimelinePage.MakeIndex(args[1]);
+        }
+        else
+        {
+            Console.WriteLine("ERROR: Argument required (path of folder containing 2P folders)");
+        }
+
+        Console.WriteLine("press ENTER to exit...");
+        _ = Console.ReadLine();
+    }
+
+    private static void DeveloperAnalysis()
     {
         // force reanalysis of a single 2P folder
         //Analysis.AnalyzeFolder(@"X:\Data\zProjects\Oxytocin Biosensor\experiments\bath apply OXT\TSeries-05102022-1208-1948", overwrite: true);
 
         string[] folderPaths =
         {
-            @"X:\Data\zProjects\Oxytocin Biosensor\experiments\bath apply OXT",
-            @"X:\Data\zProjects\Oxytocin Biosensor\experiments\electrical stimulation",
-            @"X:\Data\zProjects\Oxytocin Biosensor\experiments\patch clamp stimulation",
+            //@"X:\Data\zProjects\Oxytocin Biosensor\experiments\bath apply OXT",
+            //@"X:\Data\zProjects\Oxytocin Biosensor\experiments\electrical stimulation\2p",
+            @"X:\Data\zProjects\Oxytocin Biosensor\experiments\patch clamp stimulation\2p",
+            @"X:\Data\zProjects\Oxytocin Biosensor\experiments\ChR2 stimulation\2p",
         };
 
         foreach (string folderPath in folderPaths)
-            AnalyzeFolder(folderPath, overwrite: true);
-    }
-
-    public static void AnalyzeFolder(string folderPath, bool overwrite)
-    {
-        Analysis.AnalyzeAllSubfolders(folderPath, overwrite);
-        TimelinePage.MakeIndex(folderPath);
+        {
+            Analysis.AnalyzeAllSubfolders(folderPath, overwrite: false);
+            TimelinePage.MakeIndex(folderPath);
+        }
     }
 }
