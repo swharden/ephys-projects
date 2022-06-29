b3210534c0df0e09e843b610a7fe8cff32a4e0af
Scott W Harden <swharden@gmail.com>
2022-06-29 14:28:10 -0400

    Report2P: deploy templates

---
+++ tools/Report2P/Templates/TemplatePaths.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Report2P.Templates
+{
+    public static class TemplatePaths
+    {
+        public static string GetTemplateFolder()
+        {
+            string exePath = System.Reflection.Assembly.GetEntryAssembly()?.Location
+                ?? throw new InvalidOperationException();
+
+            string appFolder = Path.GetDirectoryName(exePath)
+                ?? throw new InvalidOperationException();
+
+            string templateFolder = Path.Combine(appFolder, "Templates");
+
+            if (!Directory.Exists(templateFolder))
+                throw new DirectoryNotFoundException(templateFolder);
+
+            return templateFolder;
+        }
+    }
+}
