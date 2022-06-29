d1af27304307151c93b7f7a28d97dd6b9fa41b51
Scott W Harden <swharden@gmail.com>
2022-06-29 14:28:17 -0400

    Report2P: update deployment script

--- tools/Report2P/deploy.bat
+++ tools/Report2P/deploy.bat
@@ -1,3 +1,4 @@
 rmdir /S /Q X:\Software\Report2P
-robocopy bin\Debug\net6.0-windows X:\Software\Report2P /E /NJH /NFL /NDL
+dotnet build --configuration Release
+robocopy bin\Release\net6.0-windows X:\Software\Report2P /E /NJH /NFL /NDL
 pause
