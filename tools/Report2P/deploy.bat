5e5456ded91f949c0e135d0e0c2b134f36cd9f8d
Scott W Harden <swharden@gmail.com>
2022-07-14 09:58:18 -0400

    deploy self-contained and trimmed

--- tools/Report2P/deploy.bat
+++ tools/Report2P/deploy.bat
@@ -1,4 +1,4 @@
 rmdir /S /Q X:\Software\Report2P
-dotnet build --configuration Release
-robocopy bin\Release\net6.0-windows X:\Software\Report2P /E /NJH /NFL /NDL
+dotnet publish --configuration Release --self-contained true --runtime win-x86
+robocopy bin\Release\net6.0-windows\win-x86 X:\Software\Report2P /E /NJH /NFL /NDL
 pause
