bbb43526bdb6076ae60c0901451f0117d2d52118
Scott W Harden <swharden@gmail.com>
2022-10-13 16:03:06 -0400

    StackProjector: accept command line argument

--- tools/StackProjector/Program.cs
+++ tools/StackProjector/Program.cs
@@ -1,13 +1,27 @@
 ﻿// This program creates maximum intensity projections for ZSeries folders
 // created by PrairieView and saves the output as a PNG file in the References subfolder.
 
-string folder = @"X:\Data\zProjects\Oxytocin Biosensor\experiments\patch clamp stimulation\2022-09-30 rat\MCN3";
-ProjectAll(folder);
+if (args.Length == 1)
+{
+    if (Directory.Exists(args[0]))
+    {
+        ProjectAll(args[0]);
+    }
+    else
+    {
+        throw new DirectoryNotFoundException(args[0]);
+    }
+}
+else
+{
+    Console.WriteLine("Arugment required: path to folder containing ZSeries sub-folders");
+}
+
 
 void ProjectAll(string folder)
 {
     string[] folderPaths = Directory.GetDirectories(folder, "ZSeries-*");
-    foreach(string folderPath in folderPaths)
+    foreach (string folderPath in folderPaths)
     {
         Project(folderPath, 1);
         Project(folderPath, 2);
@@ -23,8 +37,10 @@ void Project(string folder, int channel = 1)
     SciTIF.Image projection = stack.ProjectMax();
     projection /= 16;
 
-    string saveFolder = Path.Combine(folder, "References");
-    string saveAs = Path.Combine(saveFolder, $"projection_Ch{channel}.png");
-    Console.WriteLine(saveAs);
-    projection.Save(saveAs);
+    string referencesFolderPath = Path.Combine(folder, "References");
+	string parentFolderPath = Path.GetDirectoryName(folder) ?? string.Empty;
+    string folderName = Path.GetFileName(folder);
+    string saveFilename = $"{folderName}_projection_Ch{channel}.png";
+    projection.Save(Path.Combine(referencesFolderPath, saveFilename));
+    projection.Save(Path.Combine(parentFolderPath, saveFilename));
 }
---
+++ tools/StackProjector/run.bat
@@ -0,0 +1,2 @@
+dotnet run "X:\Data\zProjects\Oxytocin Biosensor\experiments\patch clamp stimulation\2022-10-12\2P\cell 2"
+pause
